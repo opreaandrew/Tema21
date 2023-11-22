3ad882ee803124cc4e459ee0b436346d52a62839
Andrei Oprea <opreaandrew@yahoo.com>
2023-11-22 18:19:48 +0200

    Stuff's broken

--- src/main/java/ro/fasttrackit/course20/homework/model/Transaction.java
+++ src/main/java/ro/fasttrackit/course20/homework/model/Transaction.java
@@ -1,14 +1,37 @@
 package ro.fasttrackit.course20.homework.model;
 
-import lombok.Builder;
-import lombok.With;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
+@Data
 @Builder
 @With
-public record Transaction(
-        long id,
-        String product,
-        TransactionType type,
-        double amount
-) {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String product;
+    TransactionType type;
+    double amount;
+
+    public long id() {
+        return id;
+    }
+
+    public String product() {
+        return product;
+    }
+
+    public TransactionType type() {
+        return type;
+    }
+
+    public double amount() {
+        return amount;
+    }
 }
